c9aa10e0a60092a95219919c56936a270299fcd9
PashkovGrigoriy <gripas2006@gmail.com>
2025-10-19 17:54:21 +0500

    1

---
+++ Tyuiu.FrankK.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.FrankK.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Франк";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет");
+            Console.ReadKey();
+        }
+    }
+}
