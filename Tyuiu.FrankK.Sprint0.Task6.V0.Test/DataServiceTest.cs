0fdff51663e0252541aa135c3da4b1c77d3fa3bb
PashkovGrigoriy <gripas2006@gmail.com>
2025-10-26 17:34:03 +0500

    Task6

--- Tyuiu.FrankK.Sprint0.Task6.V0.Test/DataServiceTest.cs
+++ Tyuiu.FrankK.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -1,11 +1,28 @@
-﻿namespace Tyuiu.FrankK.Sprint0.Task6.V0.Test
+﻿using Tyuiu.FrankK.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.FrankK.Sprint0.Task6.V0.Test
 {
     [TestClass]
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckAdditionArrayValid()
         {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        public void CheckSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        public void CheckMultiplicationArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
         }
     }
 }
--- Tyuiu.FrankK.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.FrankK.Sprint0.Task6.V0/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.FrankK.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.FrankK.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элеиентов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
