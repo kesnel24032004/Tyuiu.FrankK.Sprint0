48eb709812da56b68b6d43018589e8cf33609d6a
PashkovGrigoriy <gripas2006@gmail.com>
2025-10-26 17:41:06 +0500

    Task7

---
+++ Tyuiu.FrankK.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.FrankK.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.FrankK.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArraysValid()
+        {
+            int[] nums1 = { 1, 1, 1, 1, 1 };
+            int[] nums2 = { 2, 2, 2, 2, 2 };
+            int[] nums3 = { 3, 3, 3, 3, 3 };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEquivalent(nums3, res);
+        }
+    }
+}
--- Tyuiu.FrankK.Sprint0.Task7.V0/Program.cs
+++ Tyuiu.FrankK.Sprint0.Task7.V0/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+using Tyuiu.FrankK.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.FrankK.Sprint0.Task7.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Значение элементов массива 1");
+            for (int i = 0; i < arrayNums1.Length; i++)
+            {
+                Console.Write(arrayNums1[i] + ", ");
+            }
+            Console.WriteLine();
+
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Значение элементов массива 2");
+            for (int i = 0; i < arrayNums2.Length; i++)
+            {
+                Console.Write(arrayNums2[i] + ", ");
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                           ");
+            
+            if (arrayNums1.Length == arrayNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+
+                Console.WriteLine("Сумма элементов массива равна: ");
+                for (int i = 0; i < resultArray.Length; i++)
+                {
+                    Console.Write(resultArray[i] + ", ");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Исходные массивы имеют разное количество элементов");
+            }
+            Console.ReadKey();
+
+        }
+    }   
+}
